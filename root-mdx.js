bc01e3f5ce694f806ea648d0d6e7203c6bdd3131
Cat Boxer <thecatboxer@gmail.com>
2021-02-17 18:43:08 -0800

    add theme

--- root-mdx.js
+++ root-mdx.js
@@ -1,17 +1,23 @@
-// import React from 'react'
-// import {MDXProvider} from '@mdx-js/react'
-// import {Headings, Code, Blockquote, PrismSetup} from './src/components/Complete'
+import React from 'react'
+import {MDXProvider} from '@mdx-js/react'
+import {Headings, Code, Blockquote, PrismSetup} from './src/components/Complete'
+import {GlobalStyle} from "./src/themes/global-style";
+import {ThemeProvider} from 'styled-components'
+import { lightTheme, darkTheme} from "./src/themes/Themes";
 
-// const components = {
-// h2: Headings.myH2,
-// h4: Headings.myH4,
-// inlineCode: Code,
-// blockquote: Blockquote,
-// pre: PrismSetup,
-// }
+const components = {
+h2: Headings.myH2,
+h4: Headings.myH4,
+inlineCode: Code,
+blockquote: Blockquote,
+pre: PrismSetup,
+}
 
-// export const wrapMDX = ({element}) => {
-//   return <MDXProvider components={components}> 
-//   {element}
-//   </MDXProvider>
-// }
+export const wrapMDX = ({element}) => {
+  return <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+  <MDXProvider components={components}> 
+  <GlobalStyle/>
+  {element}
+  </MDXProvider>
+  </ThemeProvider>
+}
